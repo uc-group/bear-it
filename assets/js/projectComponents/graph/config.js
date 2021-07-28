7fa88a673b5f3ea03d80bf2942dd82d4a24fb42d
gbalcewicz <grzegorz@balcewicz.pl>
2021-07-28 21:43:35 +0200

    Added maroco-editor, mermaid libs

--- assets/js/projectComponents/graph/config.js
+++ assets/js/projectComponents/graph/config.js
@@ -3,6 +3,6 @@ export default {
   description: 'Flowcharts, sequence diagrams, class diagrams, etc.',
   icon: 'mdi-graph',
   menu: [
-    { label: 'callback with project', link: (project) => ({ name: 'project_details', params: { id: project.id, tab: 'members' } }) }
+    { label: 'Graphs', link: { name: 'graph_index' } , icon: 'mdi-graph' }
   ]
 }
---
+++ assets/js/projectComponents/graph/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page">
+    <monaco-editor v-model="code" language="markdown" class="page__editor"></monaco-editor>
+    <mermaid v-model="code"></mermaid>
+  </div>
+</template>
+
+<script>
+import MonacoEditor from '../../../components/MonacoEditor'
+import Mermaid from '../../../components/Mermaid'
+
+export default {
+  components: {Mermaid, MonacoEditor},
+  data() {
+    return {
+      code: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  display: flex;
+  flex-direction: column;
+
+  &__editor {
+    border: 2px solid #aaa;
+    border-radius: 5px;
+    height: 40vh;
+  }
+}
+</style>
