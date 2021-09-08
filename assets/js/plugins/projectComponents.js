e764cd02f7a65b092976ccc721999003b917349c
Paweł Goca <pawel@goca.me>
2021-09-08 19:55:57 +0200

    Revert "Changed dynamic plugin discovery to manual definition"
    This reverts commit 349d2efe01e6b3f2812a4b590bd1ccf30707a342.

--- assets/js/plugins/projectComponents.js
+++ assets/js/plugins/projectComponents.js
@@ -1,16 +1,12 @@
 import store from '~/store'
-import chatRouting from '@plugins/chat/routing';
-import chatConfig from '@plugins/chat/config';
-import graphConfig from '@plugins/graph/config';
-import pagesConfig from '@plugins/pages/config';
-import testRouting from '@plugins/test-component/routing';
-import testConfig from '@plugins/test-component/config';
-
 const components = {};
 const routes = [];
 
-const registerModule = (moduleName, { config, routes: moduleRoutes }) => {
-  if (moduleRoutes) {
+const configureRoutes = (r) => {
+  r.keys().forEach((key) => {
+    const moduleRoutes = r(key).default
+    const moduleName = key.split('/')[1]
+
     moduleRoutes.forEach((route) => {
       route.path = `${moduleName}${route.path}`
       if (route.name) {
@@ -40,23 +36,24 @@ const registerModule = (moduleName, { config, routes: moduleRoutes }) => {
           console.error(e)
         }
       }
-      console.log(route)
       routes.push(route)
     })
-  }
+  })
 
-  if (config) {
-    components[moduleName] = {
-      id: moduleName,
-      ...config
-    }
-  }
 }
 
-registerModule('chat', {config: chatConfig, routes: chatRouting});
-registerModule('graph', {config: graphConfig});
-registerModule('pages', {config: pagesConfig});
-registerModule('test-component', {config: testConfig, routes: testRouting});
+const configureComponents = (r) => {
+  r.keys().forEach((key) => {
+    const moduleName = key.split('/')[1]
+    components[moduleName] = {
+      id: moduleName,
+      ...r(key).default,
+    }
+  })
+}
+
+configureComponents(require.context('../projectComponents', true, /config.js/))
+configureRoutes(require.context('../projectComponents', true, /(routing\/index.js|routing.js)/))
 
 export const getProjectComponents = () => components;
 
